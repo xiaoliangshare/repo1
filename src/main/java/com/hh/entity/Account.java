5291c052b836c3a68579f7c6829fed0397033722
lianghaohong <lianghaohong@meituan.com>
2021-07-09 11:04:43 +0800

    创建Account实体

---
+++ src/main/java/com/hh/entity/Account.java
@@ -0,0 +1,11 @@
+package com.hh.entity;
+
+/**
+ * @description:
+ * @author: lianghaohong
+ * @date: 2021/7/9
+ * @time: 11:03 上午
+ * Copyright (C) 2021 Meituan All rights reserved
+ */
+public class Account {
+}
