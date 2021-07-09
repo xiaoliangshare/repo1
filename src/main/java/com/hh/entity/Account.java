b924de22dd9da008c0b458d391d3346e9798591f
lianghaohong <lianghaohong@meituan.com>
2021-07-09 13:48:48 +0800

    Account实体创建

--- src/main/java/com/hh/entity/Account.java
+++ src/main/java/com/hh/entity/Account.java
@@ -1,5 +1,8 @@
 package com.hh.entity;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+
 /**
  * @description:
  * @author: lianghaohong
@@ -7,5 +10,31 @@ package com.hh.entity;
  * @time: 11:03 上午
  * Copyright (C) 2021 Meituan All rights reserved
  */
-public class Account {
+public class Account implements Serializable {
+    private String name;
+    private BigDecimal money;
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "name='" + name + '\'' +
+                ", money=" + money +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
 }
