d2ea8c4dd1283dfce7cfdc79d13a6f8685aaafe9
lianghaohong <lianghaohong@meituan.com>
2021-07-09 10:56:33 +0800

    需求1.3

---
+++ src/main/java/com/hh/entity/User.java
@@ -0,0 +1,11 @@
+package com.hh.entity;
+
+/**
+ * @description:
+ * @author: lianghaohong
+ * @date: 2021/7/9
+ * @time: 10:56 上午
+ * Copyright (C) 2021 Meituan All rights reserved
+ */
+public class User {
+}
