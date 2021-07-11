1e62a0af12100e9c0926275b6f5818dda10ce7f7
lianghaohong <lianghaohong@meituan.com>
2021-07-11 21:26:21 +0800

    添加User实体类

--- src/main/java/com/hh/entity/User.java
+++ src/main/java/com/hh/entity/User.java
@@ -1,5 +1,8 @@
 package com.hh.entity;
 
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * @description:
  * @author: lianghaohong
@@ -7,5 +10,51 @@ package com.hh.entity;
  * @time: 10:56 上午
  * Copyright (C) 2021 Meituan All rights reserved
  */
-public class User {
+public class User implements Serializable {
+    private String username;
+    private String password;
+    private String sex;
+    private Date birthday;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", sex='" + sex + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
 }
